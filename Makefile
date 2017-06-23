5822400b82603d3167b0456f2fb88f129a48390b
Binbin Tang (唐宾宾) <tangbinbin@le.com>
2017-06-23 16:16:24 +0800

    fix

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ export GOPATH
 
 dep:
 	go get github.com/tangbinbin/tlog
+	go get github.com/BurntSushi/toml
 
 install:dep
 	go install im 
--- src/im/main.go
+++ src/im/main.go
@@ -1,6 +1,43 @@
 package main
 
-import ()
+import (
+	"flag"
+	"github.com/BurntSushi/toml"
+	"github.com/tangbinbin/tlog"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+)
+
+var (
+	configFile = flag.String("C", "./conf/im.conf", "config file")
+	config     *Config
+	s          *Server
+)
 
 func main() {
+	flag.Parse()
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	config = new(Config)
+	if _, err := toml.DecodeFile(*configFile, config); err != nil {
+		return
+	}
+	tlog.Init(config.Log)
+	tlog.Infof("%+v", config)
+
+	if err := newServer(); err != nil {
+		tlog.Info(err.Error())
+		return
+	}
+
+	s.start()
+
+	exit := make(chan os.Signal, 1)
+	signal.Notify(exit, syscall.SIGTERM, syscall.SIGINT)
+	<-exit
+	s.Close()
+	tlog.Close()
 }
